61bf7957e6924acad51557030715f23a4416c005
DESKTOP-O90QIGB\agung <agung.aka@gmail.com>
2018-07-03 22:48:57 +0700

    kedua

--- app/src/main/java/com/akaprod/agung/json2/MainActivity.java
+++ app/src/main/java/com/akaprod/agung/json2/MainActivity.java
@@ -3,24 +3,67 @@ package com.akaprod.agung.json2;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hasil = (TextView) findViewById(R.id.hasil);
+
+        String address = "https://owlbot.info/api/v1/dictionary/owl?format=json";
+
+        new requestData().execute(address);
     }
 
     private class requestData extends AsyncTask<String,String,String>{
         @Override
         protected String doInBackground(String... strings) {
-            return null;
+
+            String returnValue = null;
+            StringBuilder sb = null;
+            URL obj = null;
+            try{
+                obj = new URL(strings[0]);
+                HttpURLConnection con = (HttpsURLConnection) obj.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                sb = new StringBuilder();
+                while ((inputLine = in.readLine()) != null){
+                    sb.append(inputLine);
+                }
+                in.close();
+            } catch(MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            return sb.toString();
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+
+            hasil.setText(s);
         }
     }
 }
